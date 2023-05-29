ad48afbf3aaaf8411ea81d537864edc6a5efb74d
EdGavi <gavioli80@hotmail.com>
2023-05-29 12:20:41 -0300

    Atualização

--- Front/ProEventos-App/src/app/components/user/login/login.component.ts
+++ Front/ProEventos-App/src/app/components/user/login/login.component.ts
@@ -21,15 +21,15 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
 
   public login(): void {
-    this.accountService.login(this.model).subscribe(
-      () => {
-        this.router.navigateByUrl('/dashboard');
-      },
-      (error: any) => {
-        if (error.status == 401)
-          this.toaster.error('usuário ou senha inválido');
-        else console.error(error);
-      }
+this.accountService.login(this.model).subscribe(
+ () => {
+this.router.navigateByUrl('/dashboard');
+     },
+     (error: any) => {
+     if (error.status == 401)
+       this.toaster.error('usuário ou senha inválido');
+     else console.error(error);
+    }
     );
   }
 }
